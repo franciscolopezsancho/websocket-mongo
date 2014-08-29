002d452cad24f1d15335eae76dd4fe2c8130120a
Francisco <franciscolopezsancho@gmail.com>
2014-08-29 15:09:52 +0100

    presenting whole list and new data, missing putting in the first row and display real data

--- server.js
+++ server.js
@@ -77,24 +77,31 @@ wss.on('connection', function(ws) {
    ws.send(JSON.stringify({"last":document}), function() { /* ignore errors */ });
    console.log("after reques" +document.aloha);
   });
-  ws.send(JSON.stringify({"all":"mosquis"}));
+ 
+  retriveAll(function(send){ ws.send(JSON.stringify({"all":send}));})
   console.log('started client interval');
   ws.on('close', function() {
     console.log('stopping client interval');
-    clearInterval(id);
+    //clearInterval(id); whats this??? for the timestamp maybe?
   });
 });
 
 
 
 
-var retriveAll = function(){
+var retriveAll = function(sendto){
 require('mongodb').MongoClient.connect('mongodb://localhost/test', function(err, db){
     
     // make sure you have created capped collection "messages" on db "test"
 	console.log("going for ALALAALALAL colletcion");
+ var result = [];
 
- 
+   db.collection('messages').find().sort({$natural: -1}).toArray(function(err, docs) {
+        //console.log(docs)
+		//result.push(docs)
+		sendto(docs)
+        db.close();
+      });
       // seek to latest object
-      console.log( db.collection('messages').find());
+	   console.log(result)
 })};
